d93ccdd67b385e7345f813aaed7ffe6d6f750b4d
Michael Pontus <m.pontus@gmail.com>
2024-07-14 18:20:23 +0000

    Checking in

--- custom.el
+++ custom.el
@@ -3,8 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(safe-local-variable-values
-   '((org-src-preserve-indentation . t)
+   '((eval add-hook 'afer-save-hook 'org-babel-tangle nil 'local)
+     (org-src-preserve-indentation . t)
      (eval add-hook 'after-save-hook #'doom/reload 0 'local)
      (eval add-to-list 'separedit-block-regexp-plists
            '(:header "@example" :body "  " :footer ".*\\'" :keep-footer t)
@@ -12,7 +14,7 @@
      (eval add-hook 'after-save-hook 'org-babel-tangle nil 'local)
      (eval add-hook 'after-save-hook 'recompile nil 'local)
      (eval add-hook 'after-save-hook 'doom/reload 0 'local)))
- '(warning-suppress-types '((after-save-hook))))
+ '(warning-suppress-types '((after-save-hook)) t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
